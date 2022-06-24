4c1f65ee49dba2dff145560280bfd76dba14cc3a
KWY0218 <wodrk1001@gmail.com>
2022-06-24 14:57:33 +0900

    ktlint test

--- app/src/main/java/com/sopt/cilap/MainActivity.kt
+++ app/src/main/java/com/sopt/cilap/MainActivity.kt
@@ -2,10 +2,21 @@ package com.sopt.cilap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            Log.d("sdalfk","dslfjl")
+
+        
     }
+
+    fun getHigh(){
+
+    }
+
 }
