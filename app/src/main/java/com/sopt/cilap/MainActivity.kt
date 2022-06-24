df205f8532aeddc36787ac277cfd00ffd0a27f6f
KWY0218 <wodrk1001@gmail.com>
2022-06-24 15:21:32 +0900

    2nd commit

--- app/src/main/java/com/sopt/cilap/MainActivity.kt
+++ app/src/main/java/com/sopt/cilap/MainActivity.kt
@@ -4,21 +4,17 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            Log.d("sdfjk", "adskfjhdsf")
-
+        Log.d("sdfjk", "adskfjhdsf")
         Log.d("sdalfk", "dslfjl")
     }
 
     fun getHigh() {
         Log.d("sdalfk", "dslfjl")
     }
-
-    
 }
