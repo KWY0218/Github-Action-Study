4f73e12b4f411779f9723f875b91d089f38a6a35
KWY0218 <wodrk1001@gmail.com>
2022-06-24 15:18:03 +0900

    1st commit

--- app/src/main/java/com/sopt/cilap/MainActivity.kt
+++ app/src/main/java/com/sopt/cilap/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sopt.cilap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            Log.d("sdfjk", "adskfjhdsf")
     }
+
+    
 }
