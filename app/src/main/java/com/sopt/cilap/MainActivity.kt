634bc725b72f446e8d924659b6e4ccca86e54f85
강원용 <82709044+KWY0218@users.noreply.github.com>
2022-12-08 19:21:59 +0900

    Revert "ㅎㅇㅎㅇ"

--- app/src/main/java/com/sopt/cilap/MainActivity.kt
+++ app/src/main/java/com/sopt/cilap/MainActivity.kt
@@ -12,8 +12,6 @@ class MainActivity : AppCompatActivity() {
         val Abc = mutableListOf<String>()
         val GetLoadData = mutableListOf<String>()
 
-        Log.d("미안하다", "이거보여주려고 어그로")
-
         Log.d("sdfjk", "adskfjhdsf")
         Log.d("sdalfk", "dslfjl")
         Log.d("sdalfk", "dslfjl")
