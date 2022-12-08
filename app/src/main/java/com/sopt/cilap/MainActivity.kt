f9f038d0a5ff8657248bd7868f3a13ecd620e04e
KWY <wodrk1001@gmail.com>
2022-12-08 23:21:26 +0900

    [QA] tttt

--- app/src/main/java/com/sopt/cilap/MainActivity.kt
+++ app/src/main/java/com/sopt/cilap/MainActivity.kt
@@ -13,8 +13,6 @@ class MainActivity : AppCompatActivity() {
         val GetLoadData = mutableListOf<String>()
 
         Log.d("미안하다", "이거보여주려고 어그로")
-
-        Log.d("sdfjk", "adskfjhdsf")
         Log.d("sdalfk", "dslfjl")
         Log.d("sdalfk", "dslfjl")
         Log.d("sdalfk", "dslfjl")
